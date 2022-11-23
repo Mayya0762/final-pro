4c107f324cae45c0f51fb0249e64fdb7a5221380
@Mayya0762 <easymai0762@mail.ru>
2022-11-23 16:34:45 +0300

    add index

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
     const totalPrice = useSelector(getTotalPrice);
     return(<div>
         <img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png" alt="foto"/>
-        {cartItems.map(cartItem => <CartItem cartItem={cartItem}/>)}
+        {cartItems.map((cartItem, index) => <CartItem key={index} cartItem={cartItem}/>)}
         <h3>TOTAL: $ {totalPrice}</h3>
         
         </div>)
