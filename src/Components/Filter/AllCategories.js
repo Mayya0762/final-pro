5e05c2ffee4b0f37e70fc6041584758c20836186
@Mayya0762 <easymai0762@mail.ru>
2022-11-23 16:58:13 +0300

    перенос точки

--- src/Components/Filter/AllCategories.js
+++ src/Components/Filter/AllCategories.js
@@ -6,8 +6,8 @@ const AllCategories = () => {
         
         <h1>What kind of food do you like?</h1>
 
-        {['SEAFOOD', 'ITALIAN', 'APPETIZERS', 'SALADS', 'ALL'].
-        map((category, index) => <Filter key={index} category={category} />
+        {['SEAFOOD', 'ITALIAN', 'APPETIZERS', 'SALADS', 'ALL']
+        .map((category, index) => <Filter key={index} category={category} />
         )}
 
     </div>)
